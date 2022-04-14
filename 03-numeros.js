796ad3ddf161a20426483e02fbab81ab82c73526
JPablo <jpalvarez888@gmail.com>
2022-04-13 20:11:29 -0500

    Numeros

---
+++ 03-numeros.js
@@ -0,0 +1,32 @@
+
+const numero = 85;
+const numero2= 2.5;
+
+let resultado = numero + numero2;
+console.log(`La suma de ${numero} y ${numero2} es ${resultado}`);
+
+
+let n3 
+
+// Redondear hacia abajo
+Math.floor(2.6);
+
+// Redondear hacia arriba
+n3 = Math.ceil(4.6);
+
+// Raiz cuadrada
+n3 = Math.sqrt(856);
+
+// Potencia
+n3 = Math.pow(2,3);
+
+// Minimo
+n3 = Math.min(2,33,4,7,9);
+
+// Maximo
+n3 = Math.max(3,8,5,9,43,33);
+
+// Aleatorio
+n3 = Math.floor(Math.random() * 20);
+
+console.log(n3);
