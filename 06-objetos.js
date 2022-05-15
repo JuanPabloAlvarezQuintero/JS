0a4cb13cb6fd443ca1fb98bf5d4e3773f9f8ab64
JPablo <jpalvarez888@gmail.com>
2022-05-15 10:15:44 -0500

    objetos

---
+++ 06-objetos.js
@@ -0,0 +1,13 @@
+//Creando un objeto
+const producto = {
+  nombre: "TV 50 Pulgadas",
+  precio: 1200000,
+  disponible: true,
+};
+console.log(producto);
+
+//Acceder a propiedes del objeto
+console.log(producto.nombre);
+console.log(producto["disponible"]);
+
+//Agregar una propiedad al objeto despues de creado
